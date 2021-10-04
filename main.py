27af69e9856dee138984ca7ae858f5efb27beee7
TomatoCoder69 <91874599+TomatoCoder69@users.noreply.github.com>
2021-10-04 01:38:55 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+#This says/ prints the words 'Hello World!'
+print("Hello World!")
+
+#This adds two variables together.
+a = 1
+b = 2
+print(a+b)
+
+#This is an array, a list of numbers or words formed together.
+numbers = [1, 2, 3, 4]
+#This prints out the second number in the list
+print(numbers[1])
+
+#This prints out the length of the array
+letters = ["a", "b", "c", "d",]
+print(len(letters))
+
+#This assigns a definition to a piece of code
+def say_hello_world():
+    print("Hello World!")
+    
+say_hello_world()
+
+#That's gonna be it for now. I hope you found this useful for now.
